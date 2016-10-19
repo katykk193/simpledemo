06f17b26d6309a7d4c1e74ca5f3c0dc382695dc9
8A3p7 <suh9fuyp@gmail.com>
2016-10-20 00:21:33 +1100

    added user repository class

---
+++ src/main/java/com/simpledemo/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.simpledemo.repository;
+
+import com.simpledemo.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by vincent on 19/10/16.
+ */
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    //public User save(User);
+}
+
+
