325d9a6c83b5b3d3f7013c5ee8db18a3162054a9
kk <kuerten193@gmail.com>
2016-10-19 15:33:25 +1100

    add admin enrol

--- src/main/java/com/simpledemo/controller/StudentController.java
+++ src/main/java/com/simpledemo/controller/StudentController.java
@@ -1,26 +1,25 @@
-//package com.simpledemo.controller;
-//
-//import com.simpledemo.repository.LecturerRepository;
-//import com.simpledemo.repository.StudentRepository;
-//import com.simpledemo.repository.UnitRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//
-//@Controller
-//@RequestMapping("/students")
-//public class StudentController {
-//
-//    @Autowired
-//    private StudentRepository studentRepo;
-//    @Autowired
-//    private UnitRepository unitRepo;
-//
-//    @RequestMapping(value="", method= RequestMethod.GET)
-//    public String listStudentUnits(Model model){
-//        model.addAttribute("studentUnits", unitRepo.findAll());
-//        return "students/list";
-//    }
-//}
+package com.simpledemo.controller;
+
+import com.simpledemo.repository.StudentRepository;
+import com.simpledemo.repository.UnitRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/admin/enrol")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepo;
+    @Autowired
+    private UnitRepository unitRepo;
+
+    @RequestMapping(value="", method= RequestMethod.GET)
+    public String listStudents(Model model){
+        model.addAttribute("students", studentRepo.findAll());
+        return "admin/enrol/list";
+    }
+}
